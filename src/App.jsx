15cbc0b78f1abe4d52fe8af9bffbc63d3619919a
tarikuslu <huseyintarku@gmail.com>
2023-04-24 20:35:47 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,252 @@
+import { useEffect, useRef, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { themeChange } from "theme-change";
+import "./App.css";
+import Table from "./Table";
+function App() {
+  const [years, setYears] = useState([]);
+  const [typeSelector, setTypeSelector] = useState(null);
+  const [seasonSelector, setSeasonSelector] = useState(null);
+  const [raceRound, setRaceRound] = useState([]);
+  const [fetchData, setFetchData] = useState(null);
+  const [selectedRaceCount, setSelectedRaceCount] = useState(null);
+  const [close, setClose] = useState(true);
+  const [clickCount, setClickCount] = useState(0);
+  const [calendarToggle, setCalendarToggle] = useState(false);
+  const [themeToggle, setThemeToggle] = useState(true);
+  useEffect(() => {
+    const yearsCopy = handleYears();
+    setYears(yearsCopy);
+    themeChange(false);
+  }, []);
+
+  function handleYears() {
+    let yearArr = [];
+    for (let i = 1950; i <= new Date().getFullYear(); i++) {
+      yearArr = [...yearArr, i];
+    }
+    return yearArr;
+  }
+
+  async function handleSeasonChange(event) {
+    setSeasonSelector(event.target.value);
+    await fetch(
+      `https://ergast.com/api/f1/${event.target.value}/results.json?limit=500`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const raceCount = data.MRData.RaceTable.Races.length;
+        let countArr = [];
+        for (let i = 1; i <= raceCount; i++) {
+          countArr = [...countArr, i];
+        }
+        setRaceRound(countArr);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function handleTypeChange(event) {
+    event.target.value === "calendarResults"
+      ? setCalendarToggle(true)
+      : setCalendarToggle(false);
+    setTypeSelector(event.target.value);
+  }
+
+  function handleSelectedRaceCount(event) {
+    setSelectedRaceCount(event.target.value);
+  }
+
+  function handleSearch(event) {
+    event.preventDefault();
+    if (
+      typeSelector &&
+      seasonSelector &&
+      (selectedRaceCount || typeSelector === "calendarResults")
+    ) {
+      let fetchUri = "";
+      switch (typeSelector) {
+        case "raceResults":
+          fetchUri = `https://ergast.com/api/f1/${seasonSelector}/${selectedRaceCount}/results.json?limit=500`;
+          break;
+        case "qualiResults":
+          fetchUri = `https://ergast.com/api/f1/${seasonSelector}/${selectedRaceCount}/qualifying.json?limit=500`;
+          break;
+        case "calendarResults":
+          fetchUri = `https://ergast.com/api/f1/${seasonSelector}.json?limit=500`;
+          break;
+        case "driverStandings":
+          fetchUri = `https://ergast.com/api/f1/${seasonSelector}/${selectedRaceCount}/driverStandings.json?limit=500`;
+          break;
+        case "constructorStandings":
+          fetchUri = `https://ergast.com/api/f1/${seasonSelector}/${selectedRaceCount}/constructorStandings.json?limit=500`;
+          break;
+      }
+
+      fetch(fetchUri)
+        .then((response) => response.json())
+        .then((data) => {
+          setFetchData(data);
+          setClickCount((prev) => prev + 1);
+        })
+        .catch((err) => console.log(err));
+
+      setTimeout(() => {
+        handleScroll();
+      }, 750);
+    } else {
+      if (!calendarToggle) {
+        setClose(false);
+        setTimeout(() => {
+          setClose(true);
+        }, 3500);
+      }
+    }
+  }
+
+  function closeError() {
+    setClose(true);
+  }
+
+  const ref = useRef(null);
+  const topRef = useRef(null);
+  function handleScroll() {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }
+
+  function scrollToTop() {
+    topRef.current?.scrollIntoView({ behavior: "smooth" });
+  }
+
+  function toggleTheme() {
+    setThemeToggle((prev) => (prev ? false : true));
+  }
+
+  return (
+    <div
+      className="bg-[url('https://wallup.net/wp-content/uploads/2019/09/97350-formula-one-formula-1-race-racing-f-1.jpg')] bg-no-repeat bg-cover bg-center min-h-screen max-w-screen "
+      ref={topRef}
+    >
+      <div className="hero min-h-screen bg-base-200 bg-opacity-80">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <div
+              className={`alert alert-error shadow-lg mb-8 ${
+                close ? "invisible" : "visible"
+              }`}
+            >
+              <div onClick={closeError}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-current flex-shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>Error! One of your inputs not valid.</span>
+              </div>
+            </div>
+            <h1 className="text-5xl font-bold ">
+              F1 Historical Data Search App
+            </h1>
+            <p className="py-6 text-lg ">
+              You can search many detailed data up to 1950!
+            </p>
+            <div className="flex flex-col gap-4 items-center">
+              <select
+                value={typeSelector}
+                onChange={handleTypeChange}
+                className="select select-bordered w-full max-w-xs "
+              >
+                <option disabled selected>
+                  What data are you searching?
+                </option>
+                <option value={"raceResults"}>Race Results</option>
+                <option value={"qualiResults"}>Qualifying Results</option>
+                <option value={"calendarResults"}>Race Calendar</option>
+                <option value={"driverStandings"}>Driver Standings</option>
+                <option value={"constructorStandings"}>
+                  Constructor Standings
+                </option>
+              </select>
+              <select
+                value={seasonSelector}
+                onChange={handleSeasonChange}
+                className="select select-bordered w-full max-w-xs"
+              >
+                <option disabled selected>
+                  Which season?
+                </option>
+                {years.map((year) => {
+                  return (
+                    <option key={year} value={year}>
+                      {" "}
+                      {year}{" "}
+                    </option>
+                  );
+                })}
+              </select>
+              <select
+                value={selectedRaceCount}
+                onChange={handleSelectedRaceCount}
+                className="select select-bordered w-full max-w-xs"
+                disabled={calendarToggle}
+              >
+                <option disabled selected>
+                  Which round?
+                </option>
+                {raceRound.map((round) => {
+                  return (
+                    <option key={round} value={round}>
+                      Round {round}
+                    </option>
+                  );
+                })}
+              </select>
+              <button className="btn btn-wide" onClick={handleSearch}>
+                Search
+              </button>
+
+              <div className="flex items-center gap-3 ">
+                <span className="text-2xl">🌝</span>
+
+                <input
+                  type="checkbox"
+                  className="toggle toggle-lg"
+                  data-toggle-theme="dark,light"
+                  data-act-class="ACTIVECLASS"
+                />
+
+                <span className="text-2xl">🌚</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full bg-white">
+        {fetchData && (
+          <Table
+            data={fetchData}
+            dataType={typeSelector}
+            clickCount={clickCount}
+            ref={ref}
+          />
+        )}
+
+        {fetchData && (
+          <button className="btn btn-block" onClick={scrollToTop}>
+            Scroll To Top
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Table.jsx
@@ -0,0 +1,210 @@
+import React, { forwardRef, useEffect, useState } from "react";
+
+function Table(props, ref) {
+  const [headTitles, setHeadTitles] = useState([]);
+  const [bodyData, setBodyData] = useState([]);
+  const [tableTitle, setTableTitle] = useState("");
+  useEffect(() => {
+    handleDisplayData();
+  }, []);
+
+  useEffect(() => {
+    handleDisplayData();
+  }, [props.clickCount]);
+
+  const displayedBodyData = bodyData.map((bdata) => {
+    let keys = Object.keys(bdata);
+    return (
+      <tr>
+        {keys.map((key, index, arr) => {
+          return arr[index] === 0 ? (
+            <th> {bdata[key]} </th>
+          ) : (
+            <td> {bdata[key]} </td>
+          );
+        })}
+      </tr>
+    );
+  });
+
+  function handleDisplayData() {
+    console.log(props.data.MRData);
+    if (props.dataType === "raceResults") {
+      const [...res] = props.data.MRData.RaceTable.Races[0].Results;
+      let emptyArray = [];
+      for (let i = 0; i < res.length; i++) {
+        emptyArray = [
+          ...emptyArray,
+          {
+            position: res[i].position,
+            no: res[i].number,
+            driver: res[i].Driver.givenName + " " + res[i].Driver.familyName,
+            constructor: res[i].Constructor.name,
+            laps: res[i].laps,
+            grid: res[i].grid,
+            time: res[i].Time ? res[i].Time.time : " ",
+            status: res[i].status,
+            points: res[i].points,
+          },
+        ];
+      }
+      setTableTitle(
+        `${props.data.MRData.RaceTable.Races[0].raceName} - ${props.data.MRData.RaceTable.Races[0].season} - Round ${props.data.MRData.RaceTable.Races[0].round}`
+      );
+      setBodyData(emptyArray);
+      setHeadTitles([
+        "Pos",
+        "No",
+        "Driver",
+        "Constructor",
+        "Laps",
+        "Grid",
+        "Time",
+        "Status",
+        "Points",
+      ]);
+    } else if (props.dataType === "qualiResults") {
+      if (!props.data.MRData.RaceTable.Races.length) {
+        setBodyData([
+          {
+            position: " ",
+            no: " ",
+            driver: " ",
+            constructor: " ",
+            Q1: " ",
+            Q2: " ",
+            Q3: " ",
+          },
+        ]);
+        return;
+      }
+      const [...res] = props.data.MRData.RaceTable.Races[0].QualifyingResults;
+      let emptyArray = [];
+      for (let i = 0; i < res.length; i++) {
+        emptyArray = [
+          ...emptyArray,
+          {
+            position: res[i].position,
+            no: res[i].number,
+            driver: res[i].Driver.givenName + " " + res[i].Driver.familyName,
+            constructor: res[i].Constructor.name,
+            Q1: res[i].Q1 || " ",
+            Q2: res[i].Q2 || " ",
+            Q3: res[i].Q3 || " ",
+          },
+        ];
+      }
+      setTableTitle(
+        `${props.data.MRData.RaceTable.Races[0].raceName} - ${props.data.MRData.RaceTable.Races[0].season} - Round ${props.data.MRData.RaceTable.Races[0].round}`
+      );
+      setBodyData(emptyArray);
+      setHeadTitles(["Pos", "No", "Driver", "Constructor", "Q1", "Q2", "Q3"]);
+    } else if (props.dataType === "driverStandings") {
+      const [...res] =
+        props.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+      let emptyArray = [];
+      for (let i = 0; i < res.length; i++) {
+        emptyArray = [
+          ...emptyArray,
+          {
+            position: res[i].position,
+            driver: res[i].Driver.givenName + " " + res[i].Driver.familyName,
+            constructor: res[i].Constructors[0].name,
+            points: res[i].points,
+            wins: res[i].wins,
+          },
+        ];
+      }
+
+      setTableTitle(
+        `Season ${props.data.MRData.StandingsTable.season} - Round ${props.data.MRData.StandingsTable.round}`
+      );
+      setBodyData(emptyArray);
+      setHeadTitles(["Pos", "Driver", "Constructor", "Points", "Wins"]);
+    } else if (props.dataType === "constructorStandings") {
+      const [...res] =
+        props.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
+      let emptyArray = [];
+      for (let i = 0; i < res.length; i++) {
+        emptyArray = [
+          ...emptyArray,
+          {
+            position: res[i].position,
+            constructor: res[i].Constructor.name,
+            nationality: res[i].Constructor.nationality,
+            points: res[i].points,
+            wins: res[i].wins,
+          },
+        ];
+      }
+      setTableTitle(
+        `Season ${props.data.MRData.StandingsTable.season} - Round ${props.data.MRData.StandingsTable.round}`
+      );
+      setBodyData(emptyArray);
+      setHeadTitles(["Pos", "Constructor", "Nationality", "Points", "Wins"]);
+    } else if (props.dataType === "calendarResults") {
+      const [...res] = props.data.MRData.RaceTable.Races;
+      let emptyArray = [];
+      for (let i = 0; i < res.length; i++) {
+        emptyArray = [
+          ...emptyArray,
+          {
+            season: res[i].season,
+            round: res[i].round,
+            raceName: res[i].raceName,
+            date: res[i].date || " ",
+            time: res[i].time || " ",
+            sprint: res[i].Sprint ? res[i].Sprint.time : "-",
+            circuit: res[i].Circuit.circuitName,
+          },
+        ];
+      }
+      setTableTitle(
+        `Season ${props.data.MRData.RaceTable.season} Race Calendar`
+      );
+      setBodyData(emptyArray);
+      setHeadTitles([
+        "Season",
+        "Round",
+        "Race Name",
+        "Date",
+        "Time",
+        "Sprint",
+        "Circuit",
+      ]);
+    }
+  }
+
+  return (
+    <div className="overflow-x-auto">
+      <div className="bg-gray-100 ">
+        <h1
+          className="text-3xl max-[390px]:text-xl text-center py-3 font-bold "
+          ref={ref}
+        >
+          {tableTitle}
+        </h1>
+      </div>
+      <table className="table table-compact w-full">
+        <thead>
+          <tr>
+            {headTitles.map((title) => (
+              <th> {title} </th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>{displayedBodyData}</tbody>
+        <tfoot>
+          <tr>
+            {headTitles.map((title) => (
+              <th> {title} </th>
+            ))}
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
+}
+
+export default forwardRef(Table);
